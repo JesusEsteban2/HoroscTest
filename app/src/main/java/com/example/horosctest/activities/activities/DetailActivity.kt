35ba0afb33d5f96825438e05f0af754b5e14e622
mananas <jesus.esteban@outlook.com>
2024-02-19 14:01:38 +0100

    First commit 19 Feb

--- app/src/main/java/com/example/horosctest/activities/activities/DetailActivity.kt
+++ app/src/main/java/com/example/horosctest/activities/activities/DetailActivity.kt
@@ -2,17 +2,70 @@ package com.example.horosctest.activities.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.example.horosctest.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class DetailActivity : AppCompatActivity() {
     // Lateinit
-    lateinit var texto: TextView
+    lateinit var textTitle: TextView
+    lateinit var textToday:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        texto=findViewById(R.id.text1)
-        texto.text=intent.getStringExtra("HOROS_NAME")
+        textTitle = findViewById(R.id.textTit)
+        textToday = findViewById(R.id.textToday)
+
+        textTitle.text = intent.getStringExtra("HOROS_NAME")
+
+
+        // Llamada en 2º Plano
+        CoroutineScope(Dispatchers.IO).launch {
+            // Llamada en segundo plano
+            val result = horoRepo().creaURL(textTitle.text.toString())
+            // Modificar UI
+            runOnUiThread {
+            textToday.text = result
+            }
+        }
+    }
+}
+
+
+class horoRepo() {
+
+    fun creaURL(horo: String): String {
+        lateinit var connection: HttpsURLConnection
+        // Establece la URL
+        val url =
+            URL("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=$horo&day=TODAY")
+        val jsonBody=JSONObject()
+        var result="Algo"
+
+        // Crear la conexión HTTP
+        try {
+            connection = (url.openConnection() as? HttpsURLConnection)!!
+            connection.requestMethod = "GET"                            // Establecer el método GET
+            connection.setRequestProperty("Accept", "application/json") // Establecer el tipo de contenido
+            connection.doInput = true
+            connection.inputStream.read(jsonBody. as ByteArray)
+            result = jsonBody.getJSONObject("data").getString("horoscope_data").toString()
+        } catch (e: Exception)
+        {
+            Log.e("HTTP", "Error en la solicitud. ", e)
+        } finally
+        {
+            // Cerrar la conexión
+            connection?.disconnect()
+
+        }
+        return result
     }
 }
